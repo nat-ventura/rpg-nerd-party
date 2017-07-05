f44f806dab46d0957ca7e08e384d9b50f5cf9ce8
Natalie Villasana <navillasa@gmail.com>
2017-07-05 17:14:32 -0400

    list index out of range but at least no more infinite loop

--- map_maker.py
+++ map_maker.py
@@ -53,7 +53,8 @@ class Earth(Texture):
         self.size = 32
         self.width = 640
         self.height = 480
-        self.tiles = []
+        self.oldtiles = []
+        self.newtiles = []
         self.png_string = "grass"
         self.instance = Texture.load_texture(self, "graphics/" + self.png_string + ".png", self.size)
     
@@ -61,8 +62,20 @@ class Earth(Texture):
         for x in range(0, self.width, self.size):
             for y in range(0, self.height, self.size):
                 window.blit(self.instance, (x + camera_x, y + camera_y))
-                tile = self.png_string, (x + camera_x, y + camera_y)
-                self.tiles.append(tile)
+                tile = (self.png_string, (x + camera_x, y + camera_y))
+                self.oldtiles.append(tile) # set manual counter at top to find out length
+                # could be the way i'm creating new tiles in game loop-- like not making enough new tiles before
+                # copying into old tiles
+                # print everythingggg
+
+    def update(self, window, camera_x, camera_y):
+        # set manual counter and increment inside the y loop
+        tile_count = 0            
+        for x in range(0, self.width, self.size):
+            for y in range(0, self.height, self.size):
+                self.png_string = self.oldtiles[tile_count][0]
+                window.blit(self.instance, (x + camera_x, y + camera_y))
+                tile_count += 1
 
 class Sky(Texture):
     def __init__(self, size = 300):
@@ -89,6 +102,7 @@ def main():
     selector = make_selector(earth)
     fps = FPS_Tracker()
     brush = "stone"
+    earth.make(window, camera_x, camera_y)
 
     running = True
     while running:
@@ -99,7 +113,7 @@ def main():
         # draw default sky and earth
         # self.width and self.height for earth right now are hard coded
         new_sky = sky.make(big_window, window)        
-        earth.make(window, camera_x, camera_y)
+        earth.update(window, camera_x, camera_y)
         # looks like you're trying to add the info of each `tile`
         # to a 2d list stored in world_map.prev_tiles
         # looks like: (self.instance, (camera_x, camera_y))
@@ -143,6 +157,7 @@ def main():
 
         # selector painting
         if event.type == pygame.MOUSEBUTTONDOWN:
+            print "mouse is down!"
             # done = False
             # while not done:
                 # if brush == "remove":
@@ -156,26 +171,41 @@ def main():
                 #             except:
                 #                 print "no tile there!"                
                 
-                if brush in ["grass", "water", "stone"]:
-                    earth.png_string = brush
-                    # done = True
-                    for tile in earth.tiles:
-                        # if it's in the same location and of the same type
-                        if brush != "stone": #tile[0] and (mouse_x - camera_x, mouse_y - camera_y) == tile[1]:
-                            print "nooooooo" #"that's the same type of tile"
-                            # done = True
-                        elif brush == "stone": #!= tile[0] and (mouse_x - camera_x, mouse_y - camera_y) == tile[1]:
-                            earth.tiles.remove(tile)
-                            earth.tiles.append([brush, tile[1]])
-                            # window.blit(earth.instance, (mouse_x + camera_x, mouse_y + camera_y))
-                            print "tile replaced"
-                            # done = True
-                # #             # something that i'm appending or blitting isn't working out...
+            if brush in ["grass", "water", "stone"]:
+                earth.png_string = brush
+                # done = True
+                earth.newtiles = []
+                
+                for tile in earth.oldtiles:
+                    print tile
+                    is_not_stone = tile[0] != "stone" 
+                    is_clicked_tile = tile[1] == (mouse_x - camera_x, mouse_y - camera_y)
+                    print is_not_stone, "is not stone"
+                    print is_clicked_tile, "is clicked tile"
+
+                    if is_not_stone and is_clicked_tile:
+                        print "*\n\n\n*****\n\n\n***"
+                        earth.newtiles.append(("stone", tile[1]))
+                    else:
+                        earth.newtiles.append(("stone", tile[1]))
+                    # # if it's in the same location and of the same type
+                    # if brush != "stone": #tile[0] and (mouse_x - camera_x, mouse_y - camera_y) == tile[1]:
+                    #     print "nooooooo" #"that's the same type of tile"
+                    #     # done = True
+                    # elif brush == "stone": #!= tile[0] and (mouse_x - camera_x, mouse_y - camera_y) == tile[1]:
+                    #     earth.tiles.remove(tile)
+                    #     earth.tiles.append([brush, tile[1]])
+                    #     # window.blit(earth.instance, (mouse_x + camera_x, mouse_y + camera_y))
+                    #     print "tile replaced"
+                    #     # done = True
+            # #             # something that i'm appending or blitting isn't working out...
 
         # # draw map
         # for x in range(0, world_map.width, world_map.size):
         #     for y in range(0, world_map.height, world_map.size):
         #         window.blit(texture.instance, (x + camera_x, y + camera_y))
+
+        earth.oldtiles = earth.newtiles[:] # makes a copy instead of pointing to the same thing
 
         # draw highlighter
         window.blit(selector, (mouse_x, mouse_y))
