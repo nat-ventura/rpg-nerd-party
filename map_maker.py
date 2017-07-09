d09a9a483465e0b75ddc8cbceb4b12701c071a9a
Natalie Villasana <navillasa@gmail.com>
2017-07-08 21:08:34 -0400

    added print statements to .make and .update

--- map_maker.py
+++ map_maker.py
@@ -46,10 +46,11 @@ class Earth(Texture):
                 window.blit(self.instance, (x + camera_x, y + camera_y))
                 tile = (self.png_string, (x + camera_x, y + camera_y))
                 self.oldtiles.append(tile)
-                tile_count += 0 # set manual counter at top to find out length
+                tile_count += 1 # set manual counter at top to find out length
                 # could be the way i'm creating new tiles in game loop-- like not making enough new tiles before
                 # copying into old tiles
                 # print everythingggg
+                print "initialize tile number:", tile_count, tile
 
     def update(self, window, camera_x, camera_y):
         # set manual counter and increment inside the y loop
@@ -59,6 +60,7 @@ class Earth(Texture):
                 self.png_string = self.oldtiles[tile_count][0]
                 window.blit(self.instance, (x + camera_x, y + camera_y))
                 tile_count += 1
+                print "update tile number:", tile_count, tile
 
 class Sky(Texture):
     def __init__(self, size = 300):
